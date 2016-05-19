fcea18956ed8fa21de8a34f2e72fdcf194da5210
Hack Reactor Students <accounts@hackreactor.com>
2016-05-18 17:30:30 -0700

    Progress on Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,15 +26,32 @@ module.exports = function(grunt) {
     },
 
     uglify: {
+      target: {
+        files: {
+          'public/dist/<%= pkg.name %>.min.js': ['public/client/**/*.js']
+        }
+      }
     },
 
     eslint: {
       target: [
-        // Add list of files to lint here
+        'public/client/**/*.js',
+        'app/**/*.js',
+        'app/*.js',
+        'lib/*.js'
       ]
     },
 
     cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          'styles.css': ['public/*.css']
+        }
+      }
     },
 
     watch: {
@@ -57,7 +74,7 @@ module.exports = function(grunt) {
 
     shell: {
       prodServer: {
-       // 'start': 'nodemon'  //?
+       // 
       }
     },
   });
@@ -83,20 +100,20 @@ module.exports = function(grunt) {
     'mochaTest'
   ]);
 
-  grunt.registerTask('build', [
+  grunt.registerTask('build', ['concat', 'uglify'
   ]);
 
   grunt.registerTask('upload', function(n) {
     if (grunt.option('prod')) {
-      // add your production server task here
+      grunt.task.run('build');
     } else {
       grunt.task.run([ 'server-dev' ]);
     }
   });
 
-  grunt.registerTask('deploy', [
-    // add your deploy tasks here
-  ]);
+  grunt.registerTask('deploy', function(n) {    
+    grunt.task.run(['test', 'eslint']);
+  });
 
 
 };
